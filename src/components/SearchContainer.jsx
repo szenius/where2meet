da8aded0f383d72f67784786d3fb5e9ddd1f2b88
Sze Ying <ting.szeying@gmail.com>
2020-05-30 16:20:23 +0800

    feat: autocomplete search boxes

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -1,33 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../context/ContextProvider";
-import { setSourceCoordinates } from "../context/actions";
+import { AutoComplete } from "antd";
+import "antd/dist/antd.css";
 
-// TODO: should not re-render on submit
 export const SearchContainer = () => {
   const { state, dispatch } = useContext(Context);
-
-  const onSearchHandler = (event) => {
-    if (state.services.maps === null) {
-      return;
-    }
-    dispatch(setSourceCoordinates(event.target.value));
-  };
+  const { autocomplete, geocoder, maps } = state.map.services;
+  const { lat, lng } = state.map.center;
+  const searchCenter = maps && new maps.LatLng(lat, lng);
 
   return (
-    <>
-    <form className="search-form" onSubmit={onSearchHandler}>
-      <SearchBox label={"From A"} />
-      <SearchBox label={"From B"} />
-      <input type="submit" value="where2meet" />
-    </form>
-    </>
+    maps && (
+      <div className="search-form">
+        <h2>where2meet</h2>
+        <SearchBox
+          autocomplete={autocomplete}
+          geocoder={geocoder}
+          searchCenter={searchCenter}
+        />
+        <SearchBox
+          autocomplete={autocomplete}
+          geocoder={geocoder}
+          searchCenter={searchCenter}
+        />
+      </div>
+    )
   );
 };
 
-// TODO: refactor
-export const SearchBox = ({ label }) => (
-  <div className="search-box">
-    <label className="search-box__label">{label}</label>
-    <input type="text" className="search-box__input"></input>
-  </div>
-);
+export const SearchBox = ({ autocomplete, geocoder, searchCenter }) => {
+  const [searchData, setSearchData] = useState([]);
+
+  const onSearchHandler = (value) => {
+    if (value.length > 0) {
+      const searchQuery = {
+        input: value,
+        location: searchCenter,
+        radius: 30000,
+      };
+      autocomplete.getQueryPredictions(searchQuery, (response) => {
+        if (response) {
+          setSearchData(response.map((data) => data.description));
+        }
+      });
+    }
+  };
+
+  const onSelectHandler = (value) => {
+    geocoder.geocode({ address: value }, (response) => {
+      const { location } = response[0].geometry;
+      console.log(location);
+      // TODO: dispatch add source coordinate
+      // this.props.addMarker(location.lat(), location.lng(), this.props.markerName);
+    });
+  };
+
+  return (
+    <div class=" search-box">
+      <AutoComplete
+        className="search-box__autocomplete"
+        dataSource={searchData}
+        onSearch={onSearchHandler}
+        onSelect={onSelectHandler}
+        placeholder="Address"
+      />
+    </div>
+  );
+};
